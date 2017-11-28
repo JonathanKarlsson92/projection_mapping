19ae7b16c8c870da36506e31487a9a838d6e977a
Jonathan Karlsson <jonathan.karlsson13@gmail.com>
2017-11-28 14:36:07 +0100

    Added html file for communication using ros bridg e

--- src/transformation.cpp
+++ src/transformation.cpp
@@ -34,46 +34,106 @@ void lookUp()
 {
      
 }
+// callback method for what to do with every message recieved
 void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr&msg)
 {
 	//std::list<int>
 	const float AR_WIDTH =0.055; //size of ar-cube
 	int nrOfCubes=msg->markers.size(); //number of found cubes (-1?)
-	//if(!msg->markers.empty()){
-	for(int i=0; i<nrOfCubes;i++) //repeat the same number as cubes
+
+	// Initializing difference between camera and projector hard coded, possibly find a way to do this dynamically
+	// assuming camera is on projector
+	//tf::Vector3 camera_to_projector_vector(0,0,0.2); //estimated difference in height
+	//tf::Quaternion camera_to_projector_quaternion(tf::Quaternion(0, 0, 0)); //Same orientation x y z?
+	// Listener
+	//tf::Vector3 marker_pos_world;
+	//tf::Quaternion marker_orient_world;
+	
+	//Listener projector
+	tf::Vector3 marker_pos_proj;
+	tf::Quaternion marker_orient_proj;
+
+	tf::TransformListener listener;
+	lookUp();
+	tf::StampedTransform world_to_ar_marker_6; //pose relative to world
+	tf::StampedTransform projector_to_ar_marker_6; //pose relative to world
+	try
 	{
-		int markerID=msg->markers[i].id; // id of detected marker i
-		//get the position and orientation of cube nr [i]
-		tf::Vector3 position(msg->markers[i].pose.pose.position.x, msg->markers[i].pose.pose.position.y, msg->markers[i].pose.pose.position.z);
-		tf::Quaternion quaternion(msg->markers[i].pose.pose.orientation.x,
-		msg->markers[i].pose.pose.orientation.y,
-		msg->markers[i].pose.pose.orientation.z,
-		msg->markers[i].pose.pose.orientation.w);
+		ros::Time now = ros::Time::now(); //What??
+		//listener.waitForTransform("/world", "/ar_marker_6", now, ros::Duration(3.0));
+		//listener.lookupTransform("/world", "/ar_marker_6",  ros::Time(0), world_to_ar_marker_6);
+
+		//Assigning marker id8 as projector
+		listener.waitForTransform("/ar_marker_8", "/ar_marker_6", now, ros::Duration(3.0));
+		listener.lookupTransform("/ar_marker_8", "/ar_marker_6",  ros::Time(0), projector_to_ar_marker_6);
+
+		//marker_pos_world=world_to_ar_marker_6.getOrigin(); //x position of relative to world frame
+		//marker_orient_world=world_to_ar_marker_6.getRotation();
+
+		marker_pos_proj=projector_to_ar_marker_6.getOrigin(); //x position of relative to projector
+		marker_orient_proj=projector_to_ar_marker_6.getRotation();
 		
-		//Creating the 4 corners of a box
-		//quatRotate rotates the vector according to the quaternion
-		//
-		tf::Vector3 pTopLeft=position+tf::quatRotate(quaternion, tf::Vector3(AR_WIDTH,AR_WIDTH,0));	//Signs have to be checked!!
-		tf::Vector3 pTopRight=position+tf::quatRotate(quaternion, tf::Vector3 (-AR_WIDTH,AR_WIDTH,0));
-		tf::Vector3 pBottomLeft=position+tf::quatRotate(quaternion, tf::Vector3 (-AR_WIDTH,-AR_WIDTH,0));
-		tf::Vector3 pBottomRight=position+tf::quatRotate(quaternion, tf::Vector3 (AR_WIDTH,-AR_WIDTH,0));
+		lookUp();
+		//ROS_INFO("position_x in world frame: [%f]", marker_pos_world.x());	//Why does this not work??
+		//ROS_INFO("position_y in world frame: [%f]", marker_pos_world.getY());
+		//ROS_INFO("position_z in world frame: [%f]", marker_pos_world.getZ());
+		//ROS_INFO("orientation_x in world frame: [%f]", marker_orient_world.x());
+
+		ROS_INFO("position_x in world frame: [%f]", marker_pos_proj.x());	//Why does this not work??
+		ROS_INFO("position_y in world frame: [%f]", marker_pos_proj.y());
+		ROS_INFO("position_z in world frame: [%f]", marker_pos_proj.z());
+	}
+	catch (tf::TransformException ex)
+	{
+		ROS_ERROR("%s",ex.what());
+		ros::Duration(1.0).sleep();
+	}
+
+	// end listener
+	if(!msg->markers.empty()){
+		//for(int i=0; i<nrOfCubes;i++) //repeat the same number as cubes
+		for(int i=0; i<nrOfCubes;i++) //repeat the same number as cubes
+		{
+			int markerID=msg->markers[i].id; // id of detected marker i
+			if(markerID!=8) //if ar-code is not the projector
+			{
+				//get the position and orientation of cube nr [i]
+				tf::Vector3 position(msg->markers[i].pose.pose.position.x, msg->markers[i].pose.pose.position.y, msg->markers[i].pose.pose.position.z);
+				tf::Quaternion quaternion(msg->markers[i].pose.pose.orientation.x,
+				msg->markers[i].pose.pose.orientation.y,
+				msg->markers[i].pose.pose.orientation.z,
+				msg->markers[i].pose.pose.orientation.w);
+		
+				//Creating the 4 corners of a box
+				//quatRotate rotates the vector according to the quaternion
+				//
+				tf::Vector3 pTopLeft=position+tf::quatRotate(quaternion, tf::Vector3(AR_WIDTH,AR_WIDTH,0));	//Signs have to be checked!!
+				tf::Vector3 pTopRight=position+tf::quatRotate(quaternion, tf::Vector3 (-AR_WIDTH,AR_WIDTH,0));
+				tf::Vector3 pBottomLeft=position+tf::quatRotate(quaternion, tf::Vector3 (-AR_WIDTH,-AR_WIDTH,0));
+				tf::Vector3 pBottomRight=position+tf::quatRotate(quaternion, tf::Vector3 (AR_WIDTH,-AR_WIDTH,0));
 
 
-		ROS_INFO("--------------------------------");
-		ROS_INFO("position_x: [%f]", position.x()); //Dist from camera
-		ROS_INFO("position_y: [%f]", position.y()); //height
-		ROS_INFO("position_z: [%f]", position.z()); //side wise
+				//ROS_INFO("--------------------------------");
+				//ROS_INFO("position_x: [%f]", position.x()); //Dist from camera
+				//ROS_INFO("position_y: [%f]", position.y()); //height
+				//ROS_INFO("position_z: [%f]", position.z()); //side wise
 
-		/*ROS_INFO("orientation_x: [%f]", quaternion.x());
-		ROS_INFO("orientation_y: [%f]", quaternion.x());
-		ROS_INFO("orientation_z: [%f]", quaternion.x());
-		ROS_INFO("orientation_z: [%f]", quaternion.x());*/
+				/*ROS_INFO("orientation_x: [%f]", quaternion.x());
+				ROS_INFO("orientation_y: [%f]", quaternion.x());
+				ROS_INFO("orientation_z: [%f]", quaternion.x());
+				ROS_INFO("orientation_z: [%f]", quaternion.x());*/
 
-		//topleftpoint
-		ROS_INFO("--------");
-		ROS_INFO("ptopleftx: [%f]", pTopLeft.x());
-		ROS_INFO("ptoplefty: [%f]", pTopLeft.y());
-		ROS_INFO("ptopleftz: [%f]", pTopLeft.z());
+				//topleftpoint
+				//ROS_INFO("--------");
+				//ROS_INFO("ptopleftx: [%f]", pTopLeft.x());
+				//ROS_INFO("ptoplefty: [%f]", pTopLeft.y());
+				//ROS_INFO("ptopleftz: [%f]", pTopLeft.z());
+
+				ROS_INFO("markerID: [%i]", msg->markers[i].id);
+			}
+		}
+		//ROS_INFO("Number of cubes: [%i]", nrOfCubes);
+		//ROS_INFO("--------------------------------");
 	}
 }
 
@@ -83,32 +143,6 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	ros::Subscriber ar_pose = n.subscribe("ar_pose_marker", 1000, poseCallback); //subscribing to position and orientation from Alvar
 
-	// Listener
-	tf::Vector3 marker_pos_world;
-	tf::Quaternion marker_orient_world;
-	tf::TransformListener listener;
-	lookUp();
-	tf::StampedTransform world_to_ar_marker_6; //pose relative to world
-	try
-	{
-		ros::Time now = ros::Time::now(); //What??
-		listener.waitForTransform("/world", "/ar_marker_6", now, ros::Duration(3.0));
-		listener.lookupTransform("/world", "/ar_marker_6",  ros::Time(0), world_to_ar_marker_6);
-		marker_pos_world=world_to_ar_marker_6.getOrigin(); //x position of relative to world frame
-		marker_orient_world=world_to_ar_marker_6.getRotation();
-		lookUp();
-		//ROS_INFO("position_x in world frame: [%f]", marker_pos_world.x());	//Why does this not work??
-		//ROS_INFO("position_y in world frame: [%f]", marker_pos_world.getY());
-		//ROS_INFO("position_z in world frame: [%f]", marker_pos_world.getZ());
-		//ROS_INFO("orientation_x in world frame: [%f]", marker_orient_world.x());
-	}
-	catch (tf::TransformException ex)
-	{
-		ROS_ERROR("%s",ex.what());
-		ros::Duration(1.0).sleep();
-	}
-
-	// end listener
 	ros::spin();
 
 	return 0;

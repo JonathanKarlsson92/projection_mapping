cdf2d208a1f5499db29fc5cb4dff529540bd30d6
Jonathan Karlsson <jonathan.karlsson13@gmail.com>
2017-12-14 22:17:20 +0100

    added custom markers for demo, also some tuning

--- src/transformation.cpp
+++ src/transformation.cpp
@@ -17,12 +17,17 @@
 // parameters that have to be set for the projector that is used
 #define view_angle_x 2*0.3410*0.85
 #define view_angle_y 2*0.1974*0.9
-//#define resolution_x 1920 //Moved to html file
-//#define resolution_y 1080 //Moved to html file
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
+#define AR_WIDTH 0.1/2
+#define AR_WIDTH_SMALL 0.07/2 //Small markers
+#define proj_angle_y 0.1974*1.25 //tilting up in radians
 
+//offset in calibration
+#define offset_x 0
+#define offset_y 0
+#define angle_x 0
+#define angle_y 0
+
+#define LOOP_RATE 10
 // Debug variables
 
 
@@ -31,7 +36,7 @@
 std::list<int> visible_markers;  //visible markers(id)
 bool visible_projector=false;	//keeps track of when projector is visible
 
-//assumes one cube
+//assumes one cube, for filter (not implemented)
 std::list<tf::StampedTransform> past_poses(5);
 
 // callback method for what to do with every message recieved
@@ -48,6 +53,7 @@ void ar_callback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr&msg)
 			for(int i=0; i<	msg->markers.size(); i++){
 				//if marker not projector, add to list of visible else set bool to true
 				if(msg->markers[i].id!=0){
+					//Since a lot of false markers are detected, the allowed markers are set to 0-10
 					if(msg->markers[i].id<10){
 						visible_markers.push_back(msg->markers[i].id);
 					}
@@ -66,6 +72,7 @@ void ar_callback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr&msg)
 }
 // Lowpass filter
 //takes pose and updates global list of poses
+//Not yet implemented!!!
 tf::StampedTransform lowPassFilter(tf::StampedTransform current_pose){
 	tf::StampedTransform mean_pose;	
 	mean_pose.setOrigin( tf::Vector3(0, 0, 0) ); //Translation to projector lens	
@@ -85,12 +92,6 @@ tf::StampedTransform lowPassFilter(tf::StampedTransform current_pose){
 tf::Vector3 from3dTo2d(tf::Vector3 corner)
 {
 	tf::Vector3 Point2d;
-
-	//offset in calibration
-	float offset_x=0;
-	float offset_y=0;
-	float angle_x=0;
-	float angle_y=0;
 	//position in x
 	float proj_const_x=1/std::tan(view_angle_x);		
 	Point2d.setX(proj_const_x*(corner.x()+offset_x)/corner.z());
@@ -114,7 +115,7 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "mapping_node");
 	ros::NodeHandle n;
 	//publisher
-	ros::Publisher ar_publisher=n.advertise<projection_mapping::Ar_projection>("ar_projection",10); 
+	ros::Publisher ar_publisher=n.advertise<projection_mapping::Ar_projection>("ar_projection",1000); 
 	projection_mapping::Transformed_marker tr_marker;
 	projection_mapping::Ar_projection msg;
 
@@ -122,6 +123,10 @@ int main(int argc, char **argv)
 	// Adding projector to tree
 	tf::TransformBroadcaster br;
   	tf::Transform transform;
+
+	//debug
+	tf::StampedTransform transform_proj;
+	tf::StampedTransform transform_marker9;
 
 	//pose relative to world
 	tf::StampedTransform projector_to_ar_marker; 
@@ -146,19 +151,31 @@ int main(int argc, char **argv)
 	tf::Vector3 marker_2D_p4;
 
 	//Loop rate(change to higher(faster) or lower(slower) publishing speed)
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(LOOP_RATE);
 
-	const float AR_WIDTH =0.045; //size of ar-cube
-	const float proj_angle_y=0.1974*1.2; //Angle of projector y
-	//const float AR_WIDTH =0.05; //for just projecting the edges of a cube
 
 	//Add projector lens relative to camera in tf
 	transform.setOrigin( tf::Vector3(0.12, 0.06, 0) ); //Translation to projector lens	
 	transform.setRotation(tf::createQuaternionFromRPY(-3.1415/2+proj_angle_y,0,3.1415));	//Rotation of coordinate system , + angle to middle
+
+	// border
+	float ar_1_border=0.01;
+	//offset for special cub(code 1) for demo
+	float ar_1_xp=0.06-ar_1_border;
+	float ar_1_xm=0.06-ar_1_border;
+	float ar_1_yp=0.13-ar_1_border;
+	float ar_1_ym=0.07-ar_1_border;
+
+	float ar_2_xp=0.157;
+	float ar_2_xm=0.04;
+	float ar_2_yp=0.043;
+	float ar_2_ym=0.85;
 	//transform.setRotation(tf::createQuaternionFromRPY(0,0,0));
 	while (ros::ok())
   	{
 		try{
+			//time measurement
+			ros::Time begin = ros::Time::now();
 			//set ros time
 			ros::Time now = ros::Time(0);
 
@@ -171,17 +188,33 @@ int main(int argc, char **argv)
 					//look in tf
 					listener.waitForTransform( "/projector_lens",marker_string(i), now, ros::Duration(1));
 					listener.lookupTransform( "/projector_lens",marker_string(i),  now, projector_to_ar_marker);
-
+					
+					
 					//get position and orientation
 					marker_pos_proj=projector_to_ar_marker.getOrigin(); //position of relative to projector
 					marker_orient_proj=projector_to_ar_marker.getRotation();
 
 					//get corners
-					pTopLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3(AR_WIDTH,AR_WIDTH,0));		
-					pTopRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,AR_WIDTH,0));
-					pBottomRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,-AR_WIDTH,0));
-					pBottomLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (AR_WIDTH,-AR_WIDTH,0));
+					//MINUS X RIGHT, MINUS Y DOWN
+					if(i==1){
+						ROS_INFO("----SPECIAL MARKER 1 DETECTED--------");
+						pTopLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3(ar_1_xm,ar_1_yp,0));		
+						pTopRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-ar_1_xp,ar_1_yp,0)); 
+						pBottomRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-ar_1_xp,-ar_1_ym,0));
+						pBottomLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (ar_1_xm,-ar_1_ym,0));
+					}else if(i==2){
+						ROS_INFO("----SPECIAL MARKER 2 DETECTED--------");
+						pTopLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3(AR_WIDTH_SMALL,AR_WIDTH_SMALL,0));		
+						pTopRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH_SMALL,AR_WIDTH_SMALL,0));
+						pBottomRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH_SMALL,-AR_WIDTH_SMALL,0));
+						pBottomLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-0.16,0.16,0));
+					}else{
+						pTopLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3(AR_WIDTH,AR_WIDTH,0));		
+						pTopRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,AR_WIDTH,0));
+						pBottomRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,-AR_WIDTH,0));
+						pBottomLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (AR_WIDTH,-AR_WIDTH,0));
 
+					}
 					//transform to 2D
 					marker_2D_p1 = from3dTo2d(pTopLeft); 
 					marker_2D_p2 = from3dTo2d(pTopRight);
@@ -220,66 +253,26 @@ int main(int argc, char **argv)
 				ar_publisher.publish(msg);
 				//clear list for next iteration			
 				msg.markers.clear();
+
+
+				//debug noise
+				listener.waitForTransform( "/camera_rgb_frame",marker_string(0), now, ros::Duration(1));
+				listener.lookupTransform( "/camera_rgb_frame",marker_string(0),  now, transform_proj);
+				listener.waitForTransform( "/camera_rgb_frame",marker_string(9), now, ros::Duration(1));
+				listener.lookupTransform( "/camera_rgb_frame",marker_string(9),  now, transform_marker9);
+				std::cout <<"noise x: "<< transform_proj.getOrigin().x()-transform_marker9.getOrigin().x()<<'\n';
+				std::cout <<"noise y: "<< transform_proj.getOrigin().y()-transform_marker9.getOrigin().y()<<'\n';
+				std::cout <<"noise z: "<< transform_proj.getOrigin().z()-transform_marker9.getOrigin().z()<<'\n';
+				std::cout <<"noise x: "<< transform_proj.getRotation().x()-transform_marker9.getRotation().x()<<'\n';
+				std::cout <<"noise y: "<< transform_proj.getRotation().y()-transform_marker9.getRotation().y()<<'\n';
+				std::cout <<"noise z: "<< transform_proj.getRotation().z()-transform_marker9.getRotation().z()<<'\n';
+				std::cout <<"noise w: "<< transform_proj.getRotation().w()-transform_marker9.getRotation().w()<<'\n';
+				
 			}
 			else{ //check from camera
 				std::cout <<"Error, projector not seen by the camera, make sure that nothing is in the way or change the lightning conditions"<<'\n';
-				//iterate over all visible markers(i.e id)
-				/*for(int i:visible_markers){
-					//look in tf
-					listener.waitForTransform( "/camera_depth_optical_frame",marker_string(i), now, ros::Duration(1));
-					listener.lookupTransform( "/camera_depth_optical_frame",marker_string(i),  now, projector_to_ar_marker);
-
-					//get position and orientation
-					marker_pos_proj=projector_to_ar_marker.getOrigin(); //position of relative to projector
-					marker_orient_proj=projector_to_ar_marker.getRotation();
-
-					//get corners
-					pTopLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3(AR_WIDTH,AR_WIDTH,0));		
-					pTopRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,AR_WIDTH,0));
-					pBottomRight=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (-AR_WIDTH,-AR_WIDTH,0));
-					pBottomLeft=marker_pos_proj+tf::quatRotate(marker_orient_proj, tf::Vector3 (AR_WIDTH,-AR_WIDTH,0));
-
-					//transform to 2D
-					marker_2D_p1 = from3dTo2d(pTopLeft); 
-					marker_2D_p2 = from3dTo2d(pTopRight);
-					marker_2D_p3 = from3dTo2d(pBottomRight);
-					marker_2D_p4 = from3dTo2d(pBottomLeft);
-
-					//printing 2 corner points
-					ROS_INFO("--------------------------------------");
-					ROS_INFO("ptopleftx: [%f]", from3dTo2d(pTopLeft).x());
-					ROS_INFO("ptoplefty: [%f]", from3dTo2d(pTopLeft).y());
-					ROS_INFO("ptopleftz: [%f]", from3dTo2d(pTopLeft).z());
-
-					ROS_INFO("pbottomrightx: [%f]", from3dTo2d(pBottomRight).x());
-					ROS_INFO("pbottomrighty: [%f]", from3dTo2d(pBottomRight).y());
-					ROS_INFO("pbottomrightz: [%f]", from3dTo2d(pBottomRight).z());
-
-					//Filling the publisher with the a message
-					tr_marker.id=i;
-					tr_marker.p1.X=marker_2D_p1.x();
-					tr_marker.p1.Y=marker_2D_p1.y();
-
-					tr_marker.p2.X=marker_2D_p2.x();
-					tr_marker.p2.Y=marker_2D_p2.y();
-
-					tr_marker.p3.X=marker_2D_p3.x();
-					tr_marker.p3.Y=marker_2D_p3.y();
-
-					tr_marker.p4.X=marker_2D_p4.x();
-					tr_marker.p4.Y=marker_2D_p4.y();
-				
-					//publish corner and id
-					msg.markers.push_back(tr_marker);
-					
-				
-				//publish list of all seen markers
-				ar_publisher.publish(msg);
-				//clear list for next iteration			
-				msg.markers.clear();
-				//END TEST
-				*/
 			}
+			std::cout <<"main time: "<<ros::Time::now()-begin<<'\n';
 		}
 		catch (tf::TransformException ex)
 		{
